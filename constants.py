5529322d75d60e5cc99578991311ce9571eba377
stcakova <s_tzakova@abv.bg>
2014-06-01 22:19:48 +0300

    constants for the entire project

---
+++ constants.py
@@ -0,0 +1,12 @@
+SIZE_X = 1000
+SIZE_Y = 720
+PART_SIZE = 40
+SCREEN_SIZE = SIZE_X, SIZE_Y
+MAP = "pictures/map"
+MOVE_PIXELS = 10
+walls = []
+
+DIRECTIONS = {'ll': 0, 'lr': 180, 'lu': -90, 'ld': 90,
+              'rl': 180, 'rr': 0, 'ru': 90, 'rd': -90,
+              'ul': 90, 'ur': -90, 'uu': 0, 'ud': 180,
+              'dl': -90, 'dr': 90, 'du': 180, 'dd': 0, }
